b45051f83d4f56473fe83953684eac590d2ab547
Alfonso Sabalbosa <fonssabalbosa.27@gmail.com>
2022-12-27 13:04:29 +0800

    adding logo for home

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+/*------------- CHANGE BACKGROUND HEADER -------------*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    if(this.scrollY >=50) header.classList.add('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader)
