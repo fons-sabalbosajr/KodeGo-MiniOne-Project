a6cf884eea6723b001d1104255be7828de22b453
Alfonso Sabalbosa <fonssabalbosa.27@gmail.com>
2022-12-28 15:19:20 +0800

    adding css styles for value ssection

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ function scrollHeader(){
 }
 window.addEventListener('scroll', scrollHeader)
 
-/*------------- SWIPER KS NAVIGATION -------------*/
+/*------------- SWIPER JS NAVIGATION -------------*/
 var swiperPopular = new Swiper(".popular__container", {
     spaceBetween: 32,
     grabCursor: true,
@@ -13,9 +13,35 @@ var swiperPopular = new Swiper(".popular__container", {
     slidesPerView: 'auto',
     loop:true,
 
-    
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-  });
+});
+
+/*------------- VALUE JS NAVIGATION -------------*/
+const accordionItems=document.querySelectorAll('.value__accordion-item')
+accordionItems.forEach((item) =>{
+    const accordionHeader = item.querySelector('.value__accordion-header')
+    accordionHeader.addEventListener('click', () =>{
+        const openItem = document.querySelector('.accordion-open')
+
+        toggleItem(item)
+
+        if(openItem && openItem !==item){
+            toggleItem(openItem)
+        }
+    })
+})
+
+const toggleItem = (item) =>{
+    const accordionContent = item.querySelector('.value__accordion-content')
+
+    if(item.classList.contains('accordion-open')){
+        accordionContent.removeAttribute('style')
+        item.classList.remove('accordion-open')
+    }else{
+        accordionContent.style.height = accordionContent.scrollHeight + 'px'
+        item.classList.add('accordion-open')
+    }
+}
