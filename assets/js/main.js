302edd28e6beffd20905fd2771e0b3f70b5f20db
Alfonso Sabalbosa <fonssabalbosa.27@gmail.com>
2022-12-28 10:11:40 +0800

    adding navigation buttons and restyle css

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,18 @@ function scrollHeader(){
     if(this.scrollY >=50) header.classList.add('scroll-header');
 }
 window.addEventListener('scroll', scrollHeader)
+
+/*------------- SWIPER KS NAVIGATION -------------*/
+var swiperPopular = new Swiper(".popular__container", {
+    spaceBetween: 32,
+    grabCursor: true,
+    centerSlides: true,
+    slidesPerView: 'auto',
+    loop:true,
+
+    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
