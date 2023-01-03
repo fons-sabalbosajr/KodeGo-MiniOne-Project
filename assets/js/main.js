7880406d47b54607b46cb60f2abcf33a071412b5
Alfonso Sabalbosa <fonssabalbosa.27@gmail.com>
2023-01-03 16:04:38 +0800

    adding scroll up, themes and breakpoint css

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,3 +45,62 @@ const toggleItem = (item) =>{
         item.classList.add('accordion-open')
     }
 }
+
+/*------------- SCROLL JS NAVIGATION -------------*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current=>{
+        const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 58,
+        sectionId = current.getAttribute('id')
+
+        if(scrollY>sectionTop && scrollY <=sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }
+        else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*------------- SCROLLING UP BUTTON -------------*/
+function scrollUp(){
+    const scrollUp= document.getElementById('scroll-up')
+    //if scroll is higher than 350 viewport height, show-scroll class to the anchor tag
+    if(this.scrollY >=350) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll')
+}
+
+window.addEventListener('scroll', scrollUp)
+
+/*------------- LIGHT AND DARK THEME -------------*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+
+//if (user is selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+//obtaining the current theme
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
+
+//validate  if the user chose the topic
+if(selectedTheme){
+    document.body.classList[selectedTHeme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+//active/deactive theme using button
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
